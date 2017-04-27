1ec445bd53e6c3149899dc35412c326da3677310
Hatim Nagarwala <a-hanaga@microsoft.com>
2017-04-27 12:05:29 +0400

    Added sales

--- MSRSales/App.xaml.cs
+++ MSRSales/App.xaml.cs
@@ -41,21 +41,23 @@ namespace MSRSales
 
 		public static void GoToMainPage()
 		{
-			Current.MainPage = new TabbedPage
-			{
-				Children = {
-					new NavigationPage(new ItemsPage())
-					{
-						Title = "Browse",
-						Icon = Device.OnPlatform("tab_feed.png", null, null)
-					},
-					new NavigationPage(new AboutPage())
-					{
-						Title = "About",
-						Icon = Device.OnPlatform("tab_about.png", null, null)
-					},
-				}
-			};
+            //Current.MainPage = new TabbedPage
+            //{
+            //	Children = {
+            //		new NavigationPage(new ItemsPage())
+            //		{
+            //			Title = "Browse",
+            //			Icon = Device.OnPlatform("tab_feed.png", null, null)
+            //		},
+            //		new NavigationPage(new AboutPage())
+            //		{
+            //			Title = "About",
+            //			Icon = Device.OnPlatform("tab_about.png", null, null)
+            //		},
+            //	}
+            //};
+
+            Current.MainPage = new Views.SalesPage();
 		}
 	}
 }
---
+++ MSRSales/Views/SalesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MSRSales.Views
+{
+    public partial class SalesPage : ContentPage
+    {
+        public SalesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
